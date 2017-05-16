10c04b4fbe06f11f26ea21078ac80efa890e17ef
Jairo <tuboi.jairo@gmail.com>
2017-05-16 17:06:23 -0300

    add project

---
+++ DropboxApiConnection/DropboxApiConnection/DropboxApiConnection.Droid/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using DropboxApiConnection;
+using System.Text;
+using System.IO;
+
+namespace DropboxApiConnection.Droid
+{
+	[Activity (Label = "DropboxApiConnection.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+        Button myButton;
+        Button button1;
+        Button button2;
+        TextView textView;
+        
+        DropboxService service;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Main);
+            
+            service = new DropboxService();
+
+            myButton = FindViewById<Button>(Resource.Id.myButton);
+            myButton.Click += Button_Click;
+            button1 = FindViewById<Button>(Resource.Id.button1);
+            button1.Click += Button1_Click;
+            button2 = FindViewById<Button>(Resource.Id.button2);
+            button2.Click += Button2_Click;
+        }
+
+        private async void Button_Click(object sender, EventArgs e)
+        {
+            myButton.Text = await service.GetCurrentAccountAsync();
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            textView = FindViewById<TextView>(Resource.Id.textView1);
+            textView.Text = await service.GetFile();
+        }
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            await service.SetFile();
+        }
+    }
+}
+
+
---
+++ DropboxApiConnection/DropboxApiConnection/DropboxApiConnection/DropboxService.cs
@@ -0,0 +1,44 @@
+﻿using Dropbox.Api;
+using Dropbox.Api.Files;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxApiConnection
+{
+    public class DropboxService
+    {
+        // https://www.dropbox.com/developers/documentation/dotnet#tutorial
+
+        string token = "";
+        string toUpload = "asdasdasdasdasdasdasdasdasdasd";
+
+        DropboxClient dropbox;
+
+        public DropboxService()
+        {
+            dropbox = new DropboxClient(token);
+        }
+
+        public async Task<String> GetCurrentAccountAsync()
+        {
+            var account = await dropbox.Users.GetCurrentAccountAsync();
+            return account.Name.DisplayName + ", " + account.Email;
+        }
+
+        public async Task<string> GetFile()
+        {
+            var file = await dropbox.Files.DownloadAsync("/data.json");
+            return await file.GetContentAsStringAsync();
+        }
+
+        public async Task SetFile()
+        {
+            var mem = new MemoryStream(Encoding.UTF8.GetBytes(toUpload));
+            var updated = await dropbox.Files.UploadAsync("/data2.txt", WriteMode.Overwrite.Instance, body: mem);
+        }
+    }
+}
